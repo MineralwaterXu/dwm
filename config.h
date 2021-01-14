64170d4e01093f06c155d3d584de3ed39397bf68
MineralwaterXu <mineralwater.xu@gmail.com>
2021-01-14 16:25:55 +0800

    change shortcut

--- config.h
+++ config.h
@@ -158,8 +158,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_y,                    togglescratch,  {.ui = 0 } },
 	{ MODKEY,                       XK_u,                    togglescratch,  {.ui = 1 } },
 	{ MODKEY,                       XK_x,                    togglescratch,  {.ui = 2 } },
-	{ MODKEY,                       XK_s,                    show,           {0} },
-	{ MODKEY,                       XK_h,                    hide,           {0} },
+	{ MODKEY|ShiftMask,             XK_s,                    show,           {0} },
+	{ MODKEY|ShiftMask,             XK_h,                    hide,           {0} },
 	TAGKEYS(                        XK_1,                                    0)
 	TAGKEYS(                        XK_2,                                    1)
 	TAGKEYS(                        XK_3,                                    2)
